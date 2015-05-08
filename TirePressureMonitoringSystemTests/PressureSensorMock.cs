f588f0acb5c8742f4172a9e0256ac9d85a1d1c6c
Miguel Cruz Zunica <miguel.cruz@birchmangroup.com>
2015-05-08 19:09:24 +0200

    add PressureSensorMock

---
+++ TirePressureMonitoringSystemTests/PressureSensorMock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDDMicroExercises.TirePressureMonitoringSystem;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystemTests
+{
+    public class PressureSensorMock : Sensor
+    {
+        private double _pressure;
+
+        public PressureSensorMock(double pressure)
+        {
+            _pressure = pressure;
+        }
+
+        public double PopNextPressurePsiValue()
+        {
+            return _pressure;
+        }
+    }
+}
