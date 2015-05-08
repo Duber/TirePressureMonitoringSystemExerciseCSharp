722482d676fb9aa4e8b1ada70abf639e81c45943
Miguel Cruz Zunica <miguel.cruz@birchmangroup.com>
2015-05-08 18:59:17 +0200

    add AlarmTest with SimpleTest

---
+++ TirePressureMonitoringSystemTests/AlarmTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystemTests
+{
+    [TestFixture]
+    public class AlarmTest
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
