bc7578c9430a28100d84d32ced2fdeab6b050118
Miguel Cruz Zunica <miguel.cruz@birchmangroup.com>
2015-05-08 19:04:56 +0200

    AlarmTest: created first test using FakeAlarm scaffolding class

--- TirePressureMonitoringSystemTests/AlarmTest.cs
+++ TirePressureMonitoringSystemTests/AlarmTest.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using TDDMicroExercises.TirePressureMonitoringSystem;
 
 namespace TDDMicroExercises.TirePressureMonitoringSystemTests
 {
     [TestFixture]
     public class AlarmTest
     {
+        public class FakeAlarm : Alarm
+        {
+            private double _pressure;
+            public FakeAlarm(double pressure)
+            {
+                _pressure = pressure;
+            }
+            protected override double GetPressure()
+            {
+                return _pressure;
+            }
+        }
+
         [Test]
         public void SimpleTest()
         {
             Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void AnAlarmIsOnWhenPressureIsSixteen()
+        {
+            var alarm = new FakeAlarm(16);
+
+            alarm.Check();
+
+            Assert.AreEqual(true, alarm.AlarmOn);
         }
     }
 }
